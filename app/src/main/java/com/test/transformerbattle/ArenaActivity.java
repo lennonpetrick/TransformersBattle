5531a5f1ebd5f20c77091f2e3751b79755d10451
lennonpetrick <lennonpetrick@gmail.com>
2018-11-24 14:55:05 -0200

    Project created

---
+++ app/src/main/java/com/test/transformerbattle/ArenaActivity.java
@@ -0,0 +1,13 @@
+package com.test.transformerbattle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArenaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arena);
+    }
+}
