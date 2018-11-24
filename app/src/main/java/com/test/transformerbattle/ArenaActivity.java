c4e92799512c3a946900cb57e3e9ec8c71111e1a
lennonpetrick <lennonpetrick@gmail.com>
2018-11-24 18:51:30 -0200

    Created Arena activity screen

--- app/src/main/java/com/test/transformerbattle/ArenaActivity.java
+++ app/src/main/java/com/test/transformerbattle/ArenaActivity.java
@@ -1,13 +1,52 @@
 package com.test.transformerbattle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
 
 public class ArenaActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.fabNewTransformer) RecyclerView mFabNewTransformer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arena);
+        createToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_action_battle: {
+                return true;
+            }
+            default : {
+                return false;
+            }
+        }
+    }
+
+    private void createToolbar() {
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
